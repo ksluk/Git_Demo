1cbeb49c2c89565a4ebde53fbebd41479f0d4e1e
Konstantin Lukin <kss.lukin@gmail.com>
2020-07-01 15:14:18 +0300

    Initial Commit

---
+++ GitDemo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  GitDemo
+//
+//  Created by ksluk on 01.07.2020.
+//  Copyright © 2020 ksluk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
